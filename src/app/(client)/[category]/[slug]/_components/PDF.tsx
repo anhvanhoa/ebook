d508104cf68376a0ff328acbafd8593922ac9ae2
Nguyễn Văn Ánh <anhvanhoa.it@gmail.com>
2025-03-24 13:50:05 +0700

    Refactor EbookPage layout and integrate PDF viewer with carousel functionality; add new PDF document

--- src/app/(client)/[category]/[slug]/_components/PDF.tsx
+++ src/app/(client)/[category]/[slug]/_components/PDF.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { useState } from 'react';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css'; // Ẩn lớp chọn văn bản
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Ẩn chú thích
-import { useCallback } from 'react';
 pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/legacy/build/pdf.worker.min.mjs', import.meta.url).toString();
 
 const options = {
@@ -11,48 +12,59 @@ const options = {
     standardFontDataUrl: '/standard_fonts/'
 };
 
-// const maxWidth = 400;
+type PageSider = {
+    pageOdds: number[];
+    pageEvens: number[];
+};
 
 const PdfViewer = ({ fileUrl }: { fileUrl: string }) => {
-    const [file, setFile] = useState('/ebook.pdf');
-    const [numPages, setNumPages] = useState();
-    const [containerRef, setContainerRef] = useState(null);
-    const [containerWidth, setContainerWidth] = useState();
-
-    const onResize = useCallback((entries) => {
-        const [entry] = entries;
-
-        if (entry) {
-            setContainerWidth(entry.contentRect.width);
-        }
-    }, []);
-
-    function onFileChange(event) {
-        const { files } = event.target;
-
-        const nextFile = files?.[0];
-
-        if (nextFile) {
-            setFile(nextFile);
-        }
+    const [pageSlide, setPageSlide] = useState<PageSider>({ pageOdds: [], pageEvens: [] });
+    function onDocumentLoadSuccess({ numPages: nextNumPages }: { numPages: number }) {
+        const slider = Array.from({ length: nextNumPages }, (_, i) => i).reduce<PageSider>(
+            (acc, el) => {
+                if (el % 2 === 0) {
+                    acc.pageOdds.push(el);
+                } else {
+                    acc.pageEvens.push(el);
+                }
+                return acc;
+            },
+            {
+                pageOdds: [],
+                pageEvens: []
+            }
+        );
+        setPageSlide(slider);
     }
-
-    function onDocumentLoadSuccess({ numPages: nextNumPages }) {
-        setNumPages(nextNumPages);
-    }
-
     return (
         <div>
-            <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {/* <Document className={'w-full'} file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}>
                 {Array.from(new Array(numPages), (el, index) => (
                     <Page
                         key={`page_${index + 1}`}
                         pageNumber={index + 1}
+                        className='*:!w-full *:!h-full'
+                        // _className='w-full'
                         // className={'w-full'}
                         // width={containerWidth ? Math.min(containerWidth, 500) : 500}
                     />
                 ))}
-            </Document>
+            </Document> */}
+            <Carousel>
+                <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+                    <CarouselContent>
+                        {pageSlide.pageOdds.length > 0 &&
+                            pageSlide.pageOdds.map((el) => (
+                                <CarouselItem key={`page_${el + 1}`}>
+                                    <div className={'grid grid-cols-2 ' + el}>
+                                        <Page pageNumber={el + 1} className='*:!w-full *:!h-full' />
+                                        {/* <Page pageNumber={el + 2} className='*:!w-full *:!h-full' /> */}
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                    </CarouselContent>
+                </Document>
+            </Carousel>
         </div>
     );
 };
--- src/app/(client)/[category]/[slug]/page.tsx
+++ src/app/(client)/[category]/[slug]/page.tsx
@@ -1,42 +1,36 @@
 import { Button } from '@/components/ui/button';
 import { Bookmark, ChevronLeft, ChevronRight, House, Type, Undo } from 'lucide-react';
 import React from 'react';
-import PDF from './_components/PDF';
 import PdfViewer from './_components/PDF';
 
 const EbookPage = async () => {
     return (
         <div className='max-w-screen-lg mx-auto my-8'>
-            <div className='grid grid-cols-2'>
-                <div className='shadow h-72 p-1'>
-                    <div className='flex items-center'>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <Undo />
-                        </Button>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <House />
-                        </Button>
-                    </div>
-                    {/* <PDF /> */}
+            <div className='flex p-1 items-center justify-between'>
+                <div>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <Undo />
+                    </Button>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <House />
+                    </Button>
                 </div>
-                <div className='shadow p-1'>
-                    <div className='flex items-center justify-end'>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <Type />
-                        </Button>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <Bookmark />
-                        </Button>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <ChevronLeft />
-                        </Button>
-                        <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
-                            <ChevronRight />
-                        </Button>
-                    </div>
-                    <PdfViewer fileUrl={'/ebook.pdf'}  />
+                <div>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <Type />
+                    </Button>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <Bookmark />
+                    </Button>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <ChevronLeft />
+                    </Button>
+                    <Button size='icon' variant={'ghost'} className='font-normal cursor-pointer !text-blue-500'>
+                        <ChevronRight />
+                    </Button>
                 </div>
             </div>
+            <PdfViewer fileUrl={'/e.pdf'} />
         </div>
     );
 };
