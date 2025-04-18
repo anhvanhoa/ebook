2a181b138563b03de5c357359f4f88f458884cc3
Nguyen Van Anh <anhvanhoa.it@gmail.com>
2025-04-18 17:07:21 +0700

    feat: enhance InfoEbook component with menu options for like, follow, share, and report functionalities

--- src/components/controll/InfoEbook.tsx
+++ src/components/controll/InfoEbook.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import { EllipsisVertical, Heart } from 'lucide-react';
+import { EllipsisVertical, Heart, Info, Share, ThumbsUp, UserPlus } from 'lucide-react';
 import { useAudio } from '@/provider/audio/context';
 import dynamic from 'next/dynamic';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
@@ -10,9 +10,41 @@ const Button = dynamic(() => import('@/components/ui/button').then((mod) => mod.
     ssr: false
 });
 
+type KeyMenu = 'like' | 'follow' | 'share' | 'report';
+
+const menus = [
+    {
+        name: 'Thích',
+        icon: ThumbsUp,
+        key: 'like'
+    },
+    {
+        name: 'Theo dõi',
+        icon: UserPlus,
+        key: 'follow'
+    },
+    {
+        name: 'Chia sẻ',
+        icon: Share,
+        key: 'share'
+    },
+    {
+        name: 'Báo cáo',
+        icon: Info,
+        key: 'report'
+    }
+];
+
 const InfoEbook = () => {
     const { audio } = useAudio();
     const { handleFavorite } = useFavoriteEbook(audio.ebook?.id ?? '');
+
+    const handleMenu = (key: KeyMenu) => () => {
+        switch (key) {
+            case 'like':
+                handleFavorite();
+        }
+    };
     if (!audio.ebook) return <div className='flex items-center gap-4 w-1/4'></div>;
     return (
         <div className='flex items-center gap-4 w-1/4'>
@@ -36,7 +68,9 @@ const InfoEbook = () => {
             </div>
             <div className='space-x-1'>
                 <Button onClick={handleFavorite} variant={'ghost'} className='size-8 !p-1 group cursor-pointer'>
-                    <Heart className={cn('fill-primary', { 'fill-rose-500 stroke-rose-500': audio.ebook.isFavorite })} />
+                    <Heart
+                        className={cn('fill-primary', { 'fill-rose-500 stroke-rose-500': audio.ebook.isFavorite })}
+                    />
                 </Button>
                 <Popover>
                     <PopoverTrigger asChild>
@@ -45,10 +79,16 @@ const InfoEbook = () => {
                         </Button>
                     </PopoverTrigger>
                     <PopoverContent className='rounded-xl w-32 p-1'>
-                        <p className='rounded-lg cursor-pointer px-3 py-1.5 text-xs hover:bg-muted'>Thích</p>
-                        <p className='rounded-lg cursor-pointer px-3 py-1.5 text-xs hover:bg-muted'>Theo dõi</p>
-                        <p className='rounded-lg cursor-pointer px-3 py-1.5 text-xs hover:bg-muted'>Chia sẻ</p>
-                        <p className='rounded-lg cursor-pointer px-3 py-1.5 text-xs hover:bg-muted'>Báo cáo</p>
+                        {menus.map((menu) => (
+                            <div
+                                onClick={handleMenu(menu.key as KeyMenu)}
+                                key={menu.key}
+                                className='flex items-center gap-2 rounded-lg cursor-pointer px-3 py-1.5 text-xs hover:bg-muted'
+                            >
+                                {<menu.icon className='size-4' />}
+                                <p>{menu.name}</p>
+                            </div>
+                        ))}
                     </PopoverContent>
                 </Popover>
             </div>
